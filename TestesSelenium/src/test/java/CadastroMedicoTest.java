5e7b7b83933e5935429f7c314d9ceefa117db9ad
renatoxd152 <renatomorillo@gmail.com>
2024-06-03 13:43:10 -0300

    test: Test to open the page and close

--- TestesSelenium/src/test/java/CadastroMedicoTest.java
+++ TestesSelenium/src/test/java/CadastroMedicoTest.java
@@ -1,4 +1,34 @@
-public class CadastroMedicoTest {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
+public class CadastroMedicoTest {
+    private WebDriver driver;
+
+    @BeforeEach
+    void setUp()
+    {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+    }
+    @AfterEach
+    void tearDown()
+    {
+        driver.quit();
+    }
+    @Test
+    @DisplayName("Should open and close the website")
+    void shouldOpenAndCloseTheWebsite() throws InterruptedException {
+        driver.get("https://cadastro-medicos-pacientes-a4n9.vercel.app");
+        Thread.sleep(1000);
+        driver.quit();
+    }
 
 }
