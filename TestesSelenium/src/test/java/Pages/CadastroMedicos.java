c4abe8757241ce28c8de8a25f188adebaf310d9a
renatoxd152 <renatomorillo@gmail.com>
2024-06-06 15:38:53 -0300

    feat: Added function to fill in fields

--- TestesSelenium/src/test/java/Pages/CadastroMedicos.java
+++ TestesSelenium/src/test/java/Pages/CadastroMedicos.java
@@ -2,10 +2,21 @@ package Pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class CadastroMedicos {
     private WebDriver driver;
     private By buttonRegisterDoctor = By.id("cadastrar");
+    private By crmField = By.id("crm");
+    private By nomeField = By.id("nome");
+    private By dataNascimentoField = By.id("data");
+    private By sexoField = By.id("sexo");
+    private By especialidadeField = By.id("especialidade");
+    private By universidadeField = By.id("universidade");
+    private By emailField = By.id("email");
+    private By telefoneField = By.id("telefone");
+
     public CadastroMedicos(WebDriver driver) {
         this.driver = driver;
     }
@@ -13,5 +24,53 @@ public class CadastroMedicos {
     public void clickRegisterDoctor() {
         driver.findElement(buttonRegisterDoctor).click();
     }
+    
+    public void fillCrmField(String crm) {
+        driver.findElement(crmField).sendKeys(crm);
+    }
+
+    public void fillNomeField(String nome) {
+        driver.findElement(nomeField).sendKeys(nome);
+    }
+
+    public void fillDataNascimentoField(String dataNascimento) {
+        driver.findElement(dataNascimentoField).sendKeys(dataNascimento);
+    }
+
+    public void fillSexoField(String sexo) {
+        driver.findElement(sexoField).sendKeys(sexo);
+    }
+
+    public void selectEspecialidade(String especialidade) {
+        WebElement especialidadeDropdown = driver.findElement(especialidadeField);
+        Select select = new Select(especialidadeDropdown);
+        select.selectByVisibleText(especialidade);
+    }
+
+    public void fillUniversidadeField(String universidade) {
+        driver.findElement(universidadeField).sendKeys(universidade);
+    }
+
+    public void fillEmailField(String email) {
+        driver.findElement(emailField).sendKeys(email);
+    }
+
+    public void fillTelefoneField(String telefone) {
+        driver.findElement(telefoneField).sendKeys(telefone);
+    }
+
+    public void fillAllFields(String crm, String nome, String dataNascimento, String sexo,
+                              String especialidade, String universidade, String email, String telefone) {
+        fillCrmField(crm);
+        fillNomeField(nome);
+        fillDataNascimentoField(dataNascimento);
+        fillSexoField(sexo);
+        selectEspecialidade(especialidade);
+        fillUniversidadeField(universidade);
+        fillEmailField(email);
+        fillTelefoneField(telefone);
+    }
+
+
 
 }
