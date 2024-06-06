d241a644f3cca510bf42a1f1e1fa7bf4d2871add
renatoxd152 <renatomorillo@gmail.com>
2024-06-06 19:05:12 -0300

    test: Should return the invalid phone number

--- TestesSelenium/src/test/java/CadastroMedicoTest.java
+++ TestesSelenium/src/test/java/CadastroMedicoTest.java
@@ -12,8 +12,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+import java.util.Random;
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -26,6 +28,27 @@ public class CadastroMedicoTest {
     private Alert alert;
     private String alertMessage;
     private Faker faker;
+    private String crm;
+    private String nome;
+    private String dataNascimento;
+    private String sexo;
+    private String universidade;
+    private String especialidade;
+    private String email;
+    private String telefone;
+    private String estado;
+    private String numero;
+    private Random random;
+
+    private static final List<String> estadosBrasileiros = Arrays.asList(
+            "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS",
+            "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC",
+            "SP", "SE", "TO"
+    );
+
+    private String getRandomEstado() {
+        return estadosBrasileiros.get(random.nextInt(estadosBrasileiros.size()));
+    }
 
     @BeforeEach
     void setUp()
@@ -38,6 +61,7 @@ public class CadastroMedicoTest {
         cadastroMedicos = new CadastroMedicos(driver);
         faker = new Faker();
         webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        random = new Random();
     }
     @AfterEach
     void tearDown()
@@ -103,14 +127,14 @@ public class CadastroMedicoTest {
         paginaInicial.clickButtonMedic();
         Thread.sleep(1000);
 
-        String crm = faker.number().digits(20);
-        String nome = faker.name().fullName();
-        String dataNascimento = "01/01/1980";
-        String sexo = faker.demographic().sex();
-        String universidade = faker.university().name();
-        String especialidade = "Cardiologista";
-        String email = faker.internet().emailAddress();
-        String telefone = faker.number().digits(11);
+        crm = faker.number().digits(20);
+        nome = faker.name().fullName();
+        dataNascimento = "01/01/1980";
+        sexo = faker.demographic().sex();
+        universidade = faker.university().name();
+        especialidade = "Cardiologista";
+        email = faker.internet().emailAddress();
+        telefone = faker.number().digits(11);
 
         cadastroMedicos.fillAllFields(crm, nome, dataNascimento, sexo, especialidade, universidade, email, telefone);
         Thread.sleep(1000);
@@ -124,6 +148,41 @@ public class CadastroMedicoTest {
         alertMessage = alert.getText();
 
         assertEquals("O CRM deve estar no formato 0000000/UF.", alertMessage);
+        Thread.sleep(1000);
+        alert.accept();
+    }
+
+    @Test
+    @DisplayName("Should show an error in the message if it is not a correct phone number")
+    void shouldShowAnErrorInTheMessageIfItIsNotACorrectPhoneNumber() throws InterruptedException{
+        driver.get(url);
+
+        paginaInicial.clickButtonMedic();
+        Thread.sleep(1000);
+
+        numero = faker.number().digits(7);
+        estado = getRandomEstado();
+        crm = numero + "/" + estado;
+        nome = faker.name().fullName();
+        dataNascimento = "01/01/1980";
+        sexo = faker.demographic().sex();
+        universidade = faker.university().name();
+        especialidade = "Cardiologista";
+        email = faker.internet().emailAddress();
+        telefone = faker.number().digits(11);
+
+        cadastroMedicos.fillAllFields(crm, nome, dataNascimento, sexo, especialidade, universidade, email, telefone);
+        Thread.sleep(1000);
+        cadastroMedicos.clickRegisterDoctor();
+        Thread.sleep(1000);
+
+        webDriverWait.until(ExpectedConditions.alertIsPresent());
+
+        alert = driver.switchTo().alert();
+
+        alertMessage = alert.getText();
+
+        assertEquals("Telefone inválido. Deve estar no formato XXXXXXXX", alertMessage);
         Thread.sleep(1000);
         alert.accept();
     }
