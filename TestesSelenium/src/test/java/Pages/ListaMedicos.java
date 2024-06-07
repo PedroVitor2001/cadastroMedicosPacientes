3f004d6008cf79b4e2b19d134ffe2840042e0fd8
renatoxd152 <renatomorillo@gmail.com>
2024-06-07 15:59:13 -0300

    feat: Added function to return the lines of table

--- TestesSelenium/src/test/java/Pages/ListaMedicos.java
+++ TestesSelenium/src/test/java/Pages/ListaMedicos.java
@@ -2,10 +2,14 @@ package Pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class ListaMedicos {
     private WebDriver driver;
     private By listaAllButton = By.xpath("//*[@id=\"cadastro-2\"]/div/button[1]");
+    private By listDoctors = By.id("medicosTable");
 
     public ListaMedicos(WebDriver driver) {
         this.driver = driver;
@@ -13,5 +17,9 @@ public class ListaMedicos {
 
     public void listAll() {
         driver.findElement(listaAllButton).click();
+    }
+
+    public List<WebElement> getDoctorRows() {
+        return driver.findElements(listDoctors);
     }
 }
