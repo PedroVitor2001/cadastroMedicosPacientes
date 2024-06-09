c8799afa063eeca2d3e90a97ecc53cbaa4be9c74
corrumptus <lazarinilucas42@gmail.com>
2024-06-09 16:23:27 -0300

    feat: setting up things for the tests

--- TestesSelenium/src/test/java/paciente/CadastroPacienteTest.java
+++ TestesSelenium/src/test/java/paciente/CadastroPacienteTest.java
@@ -1,5 +1,27 @@
 package paciente;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class CadastroPacienteTest {
-    
+    private WebDriver driver;
+
+    @BeforeEach
+    void setUp() {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+    }
 }
