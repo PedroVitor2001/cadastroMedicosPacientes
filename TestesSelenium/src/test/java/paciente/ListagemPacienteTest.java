4dfb7886e0206cf0c4450aef5ccaaa1692ba5b18
MuriloMPodenciano <murilo.m.podenciano@gmail.com>
2024-06-10 02:37:09 -0300

    test: created before and after each for lista pacientes

--- TestesSelenium/src/test/java/paciente/ListagemPacienteTest.java
+++ TestesSelenium/src/test/java/paciente/ListagemPacienteTest.java
@@ -1,12 +1,42 @@
 package paciente;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import Pages.ListaPacientes;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class ListagemPacienteTest {
     private WebDriver driver;
     private String url = "https://cadastro-medicos-pacientes-a4n9.vercel.app/pages/dadosPaciente.html";
     private ListaPacientes page;
+
+    @BeforeEach
+    void setUp() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
+        driver.get(url);
+        page = new ListaPacientes(driver);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+    }
+
+    @Nested
+    @DisplayName("Header buttons")
+    class Header{
+        
+    }
+
     
 }
