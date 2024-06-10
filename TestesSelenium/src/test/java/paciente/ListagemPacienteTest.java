fe4eeedcf8c0d38ba30fbb852599045826037983
MuriloMPodenciano <murilo.m.podenciano@gmail.com>
2024-06-10 12:28:29 -0300

    test: made tests to check if alert shows up when CPF is empty for pacientes list

--- TestesSelenium/src/test/java/paciente/ListagemPacienteTest.java
+++ TestesSelenium/src/test/java/paciente/ListagemPacienteTest.java
@@ -2,20 +2,26 @@ package paciente;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.Duration;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import Pages.ListaPacientes;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class ListagemPacienteTest {
     private WebDriver driver;
+    private WebDriverWait webDriverWait;
     private String url = "https://cadastro-medicos-pacientes-a4n9.vercel.app/pages/dadosPaciente.html";
     private ListaPacientes page;
 
@@ -24,9 +30,10 @@ public class ListagemPacienteTest {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
-
+        
         driver = new ChromeDriver(options);
         driver.get(url);
+        webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(10));
         page = new ListaPacientes(driver);
     }
 
@@ -69,7 +76,32 @@ public class ListagemPacienteTest {
     @Nested
     @DisplayName("Empty CPF")
     class EmptyCPF{
-        
+        @Test
+        @DisplayName("Should display alert 'digite o cpf' after clicking listbtn")
+        void shouldDisplayAlertOnEmptyCpfAfterClickingListButton() {
+            page.clickListBtn();
+
+            String alertText = webDriverWait.until(ExpectedConditions.alertIsPresent()).getText();
+            assertEquals("Digite o CPF", alertText);
+        }
+
+        @Test
+        @DisplayName("Should display alert 'digite o cpf' after clicking alterarbtn")
+        void shouldDisplayAlertOnEmptyCpfAfterClickingAlterarButton() {
+            page.clickEditBtn();
+
+            String alertText = webDriverWait.until(ExpectedConditions.alertIsPresent()).getText();
+            assertEquals("Digite o CPF", alertText);
+        }
+
+        @Test
+        @DisplayName("Should display alert 'digite o cpf' after clicking excluirbtn")
+        void shouldDisplayAlertOnEmptyCpfAfterClickingExcluirButton() {
+            page.clickDeleteBtn();
+
+            String alertText = webDriverWait.until(ExpectedConditions.alertIsPresent()).getText();
+            assertEquals("Digite o CPF", alertText);
+        }
     }
     
     @Nested
