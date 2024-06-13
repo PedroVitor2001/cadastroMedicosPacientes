f5f85fb7992e0ba4184c57c3bd2b8b87fd26aea2
corrumptus <lazarinilucas42@gmail.com>
2024-06-12 21:37:40 -0300

    feat: defining the urls of the pages covered by the home page

--- TestesSelenium/src/test/java/home/HomeTests.java
+++ TestesSelenium/src/test/java/home/HomeTests.java
@@ -1,5 +1,12 @@
 package home;
 
 public class HomeTests {
-    
+    private final String HOME_PAGE =
+        "https://cadastro-medicos-pacientes-a4n9.vercel.app/index.html";
+
+    private final String MEDIC_PAGE =
+        "https://cadastro-medicos-pacientes-a4n9.vercel.app/pages/medicos.html";
+
+    private final String PATIENT_PAGE =
+        "https://cadastro-medicos-pacientes-a4n9.vercel.app/pages/pacientes.html";
 }
