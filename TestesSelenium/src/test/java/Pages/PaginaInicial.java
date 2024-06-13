5f3bf621aa13e0c94a2ab7148e8508ceafb94453
corrumptus <lazarinilucas42@gmail.com>
2024-06-12 23:44:53 -0300

    revert: rollback the waited button click

--- TestesSelenium/src/test/java/Pages/PaginaInicial.java
+++ TestesSelenium/src/test/java/Pages/PaginaInicial.java
@@ -1,15 +1,23 @@
 package Pages;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class PaginaInicial {
+    private WebDriverWait wait;
+
     private WebElement buttonMedic;
     private WebElement buttonPatient;
     private WebElement buttonHome;
 
     public PaginaInicial(WebDriver driver) {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
         this.buttonMedic = driver.findElement(By.className("pagina-medicos"));
         this.buttonPatient = driver.findElement(By.className("pagina-pacientes"));
         this.buttonHome = driver.findElement(By.className("sair"));
@@ -25,5 +33,13 @@ public class PaginaInicial {
 
     public void goToTheHomePage() {
         buttonHome.click();
+    }
+
+    public void goToTheMedicPageWaited() {
+        wait.until(ExpectedConditions.elementToBeClickable(buttonMedic)).click();
+    }
+
+    public void clickPatientPageWaited() {
+        wait.until(ExpectedConditions.elementToBeClickable(buttonPatient)).click();
     }
 }
